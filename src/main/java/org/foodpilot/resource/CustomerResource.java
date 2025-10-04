8c941c3aea2485a0179888f4415faafb5da274c9
Sivaraman Raja <sivaramanr38@gmail.com>
2025-10-04 10:26:21 +0530

    Added OpenAPI doc for the CustomerResource and  get all customers API

--- src/main/java/org/foodpilot/resource/CustomerResource.java
+++ src/main/java/org/foodpilot/resource/CustomerResource.java
@@ -1,9 +1,14 @@
 package org.foodpilot.resource;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.foodpilot.dto.CustomerDTO;
 import org.foodpilot.service.CustomerService;
@@ -12,12 +17,17 @@ import java.util.List;
 
 @Tag(name = "Customer", description = "Operations related to customer data")
 @Path("/customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerResource {
 
     @Inject
     CustomerService customerService;
 
     @GET
+    @Operation(summary = "Get all customers", description = "Returns a list of all available customers")
+    @APIResponse(responseCode = "200", description = "List of restaurants")
+    @APIResponse(responseCode = "204", description = "No customer found")
     public Response getAll(){
         List<CustomerDTO> customerDTOList = customerService.getAllCustomers();
         if(customerDTOList.isEmpty()) {

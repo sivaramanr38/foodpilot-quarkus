c2119709c2b7f0727c8d794d676cd46a96af2282
Sivaraman Raja <sivaramanr38@gmail.com>
2025-10-07 23:26:10 +0530

    Added a controller get customer by id

--- src/main/java/org/foodpilot/resource/CustomerResource.java
+++ src/main/java/org/foodpilot/resource/CustomerResource.java
@@ -1,19 +1,19 @@
 package org.foodpilot.resource;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.foodpilot.dto.CustomerDTO;
+import org.foodpilot.exception.RestaurantNotFoundException;
 import org.foodpilot.service.CustomerService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Tag(name = "Customer", description = "Operations related to customer data")
 @Path("/customer")
@@ -34,5 +34,18 @@ public class CustomerResource {
             return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.ok(customerDTOList).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Operation(summary = "Get customer by ID", description = "Returns a customer by its unique ID")
+    @APIResponse(responseCode = "200", description = "Customer found")
+    @APIResponse(responseCode = "404", description = "Customer not found")
+    public Response getCustomerById(@Parameter(description = "ID of the customer", example = "101") @PathParam("id")  Long id){
+        Optional<CustomerDTO> customerDTO = customerService.getCustomerById(id);
+        if(customerDTO.isEmpty()) {
+            throw  new RestaurantNotFoundException("Customer not found with ID: " + id);
+        }
+        return Response.ok(customerDTO.get()).build();
     }
 }
